bba9cc7a9ddc369d439e758b103ba3a299971446
Emma <emmanuelalozieuwa@gmail.com>
2023-06-23 16:40:42 +0100

    Updated queryform

---
+++ querysummary.sql
@@ -0,0 +1,16 @@
+USE gamesdb;
+-- SHOW TABLES;
+
+-- SELECT DISTINCT first_name FROM customers;
+
+-- SELECT DISTINCT first_name, age FROM customers WHERE age > 15;
+
+-- SELECT DISTINCT first_name, age FROM customers WHERE age BETWEEN 17 AND 50;
+
+-- SELECT first_name, age FROM customers WHERE first_name LIKE '%S';
+
+-- SELECT first_name AS name, age FROM customers WHERE age > 15;
+
+-- SELECT first_name, age FROM customers ORDER BY age ASC;
+
+-- SELECT first_name, age FROM customers WHERE age > 15 LIMIT 2;
